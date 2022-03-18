e41996946578b84c76e8700b89ce746fbad522d1
AbdulRehman-Irfan <101065713+AbdulRehman-Irfan@users.noreply.github.com>
2022-03-18 12:19:02 +0500

    Pass

---
+++ script.js
@@ -0,0 +1,26 @@
+const skis = document.querySelectorAll(".prof-skills");
+console.log(skis)
+function reveal() {
+
+   const sk = document.getElementsByClassName("prof-container")
+   for(var i=0;i<sk.length;i++)
+   {
+       sk[i].classList.add("filer")
+   }
+    
+  
+ }
+ function revealout() {
+
+   const sk = document.getElementsByClassName("prof-container")
+   for(var i=0;i<sk.length;i++)
+   {
+       sk[i].classList.remove("filer")
+   }
+    
+  
+ }
+ skis[0].addEventListener("mouseover",reveal)
+ skis[0].addEventListener("mouseout",revealout)
+
+
