aa72c19f1b7d2ce79283e9fd9de035aa022e4d77
AbdulRehman-Irfan <101065713+AbdulRehman-Irfan@users.noreply.github.com>
2022-03-18 21:02:01 +0500

    interst

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const skis = document.querySelectorAll(".prof-skills");
-console.log(skis)
 function reveal() {
 
    const sk = document.getElementsByClassName("prof-container")
@@ -8,19 +7,20 @@ function reveal() {
        sk[i].classList.add("filer")
    }
     
-  
+
  }
  function revealout() {
-
    const sk = document.getElementsByClassName("prof-container")
    for(var i=0;i<sk.length;i++)
    {
        sk[i].classList.remove("filer")
    }
-    
-  
  }
  skis[0].addEventListener("mouseover",reveal)
  skis[0].addEventListener("mouseout",revealout)
+
+
+
+
 
 
